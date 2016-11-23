416ae1ec01887df8098aea7f88c09bf7ecc5a9da
a1655086 <a1655086@LAB209M25.lab209.campus.ifsp>
2016-11-23 20:38:35 -0200

    like Tiago :)

--- src/main/java/eagles/sabor_mel/control/ControllerEstado.java
+++ src/main/java/eagles/sabor_mel/control/ControllerEstado.java
@@ -7,26 +7,41 @@ package eagles.sabor_mel.control;
 
 import eagles.sabor_mel.dao.EstadoDAO;
 import eagles.sabor_mel.model.Estado;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
  * @author a1655086
  */
-
-
-
 public class ControllerEstado {
-    
+
     private static EstadoDAO daoEstado = new EstadoDAO();
-    
-    
-    public static String escreverEstado(String uf){
-        
+
+    public static String escreverEstado(String uf) {
+
         return daoEstado.findByUf(uf).getNome();
-        
+
     }
-    
-    public static Estado getEstadoByUf(String uf){
+
+    public static Estado getEstadoByUf(String uf) {
         return daoEstado.findByUf(uf);
+    }
+
+    public static List<Map<String, String>> listEstados() {
+
+        EstadoDAO daoEstado = new EstadoDAO();
+        List<Estado> estados = daoEstado.findAll();
+        List<Map<String, String>> listaEstados = new ArrayList<>();
+
+        for (Estado e : estados) {
+            Map<String, String> specEstado = new HashMap<>();
+            specEstado.put("uf", String.valueOf(e.getUf()));
+            listaEstados.add(specEstado);
+        }
+
+        return listaEstados;
     }
 }
--- src/main/java/eagles/sabor_mel/control/ControllerFuncionario.java
+++ src/main/java/eagles/sabor_mel/control/ControllerFuncionario.java
@@ -12,7 +12,6 @@ import eagles.sabor_mel.model.Cidade;
 import eagles.sabor_mel.model.DateGenerator;
 import eagles.sabor_mel.model.Documento;
 import eagles.sabor_mel.model.Endereco;
-import eagles.sabor_mel.model.Estado;
 import eagles.sabor_mel.model.Funcionario;
 import eagles.sabor_mel.model.Sexo;
 import eagles.sabor_mel.model.Telefone;
@@ -40,56 +39,56 @@ public class ControllerFuncionario {
     * @author Tiago Lima Villalobos
     * Função para listar todos os funcionários cadastrados.
     *
-    */
-    public static  List<Map<String, String>> listFuncionarios(){
+     */
+    public static List<Map<String, String>> listFuncionarios() {
         List<Funcionario> funcionarios = daoFuncionario.findAll();
         List<Map<String, String>> listaFuncionarios = new ArrayList<>();
-        
-        for(Funcionario f: funcionarios){
-            if(!(String.valueOf(f.getNome()).equals("Administrador"))){
+
+        for (Funcionario f : funcionarios) {
+            if (!(String.valueOf(f.getNome()).equals("Administrador"))) {
                 Map<String, String> specFuncionario = new HashMap<>();
                 specFuncionario.put("id", String.valueOf(f.getIdPessoa()));
                 specFuncionario.put("nome", String.valueOf(f.getNome()));
                 specFuncionario.put("usuario", String.valueOf(f.getUsuario()));
                 specFuncionario.put("acesso", String.valueOf(f.getAcesso()));
-                
+
                 listaFuncionarios.add(specFuncionario);
-            } 
+            }
         }
-        
+
         return listaFuncionarios;
     }
-    
-    public static  Map<String, String> searchFuncionario(Long id){
+
+    public static Map<String, String> searchFuncionario(Long id) {
         Funcionario funcionario = daoFuncionario.getById(id);
         Map<String, String> specFuncionario = new HashMap<>();
-        
+
         specFuncionario.put("idPessoa", String.valueOf(funcionario.getIdPessoa()));
         specFuncionario.put("nome", funcionario.getNome());
         specFuncionario.put("dataNascimento", DateGenerator.dateFormat(funcionario.getDataNascimento()));
         specFuncionario.put("email", funcionario.getEmail());
         specFuncionario.put("sexo", String.valueOf(funcionario.getSexo()));
         specFuncionario.put("documento", String.valueOf(funcionario.getDocumento().getNumero()));
-        
+
         specFuncionario.put("cep", String.valueOf(funcionario.getEndereco().getCep()));
         specFuncionario.put("logradouro", String.valueOf(funcionario.getEndereco().getLogradouro()));
         specFuncionario.put("numero", String.valueOf(funcionario.getEndereco().getNumero()));
         specFuncionario.put("bairro", String.valueOf(funcionario.getEndereco().getBairro().getNome()));
         specFuncionario.put("cidade", String.valueOf(funcionario.getEndereco().getBairro().getCidade().getNome()));
         specFuncionario.put("estado", String.valueOf(funcionario.getEndereco().getBairro().getCidade().getEstado().getUf()));
-        
+
         specFuncionario.put("usuario", String.valueOf(funcionario.getUsuario()));
         specFuncionario.put("acesso", String.valueOf(funcionario.getAcesso()));
-        
+
         return specFuncionario;
     }
-    
+
     /*
     * @author dhiego
     * This function calls VendaDAO and CrediarioDAO to persist a sell.
     *
-    */
-    public static Map<String, String> searchFuncionario(String nome){
+     */
+    public static Map<String, String> searchFuncionario(String nome) {
         FuncionarioDAO daoFuncionario = new FuncionarioDAO();
         Funcionario funcionario = daoFuncionario.getByName(nome);
         Map<String, String> specFuncionario = new HashMap<>();
@@ -102,26 +101,26 @@ public class ControllerFuncionario {
         specFuncionario.put("senha", funcionario.getSenha());
         return specFuncionario;
     }
-    
-    public static Map<String, String> searchFuncionario(String nome, String senha) throws NoSuchAlgorithmException, UnsupportedEncodingException{
-        
+
+    public static Map<String, String> searchFuncionario(String nome, String senha) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+
         Funcionario funcionario = daoFuncionario.getByNameSenha(nome, senha);
-        Map <String, String> specFuncionario = new HashMap<>();
-        
+        Map<String, String> specFuncionario = new HashMap<>();
+
         specFuncionario.put("nome", funcionario.getNome());
         specFuncionario.put("login", funcionario.getUsuario());
         specFuncionario.put("senha", funcionario.getSenha());
         specFuncionario.put("acesso", String.valueOf(funcionario.getAcesso()));
-        
+
         return specFuncionario;
     }
-    
-    public static List<Map<String,String>> searchUsuario(String nome){
+
+    public static List<Map<String, String>> searchUsuario(String nome) {
         List<Map<String, String>> funcionarios = new ArrayList<>();
         List<Funcionario> listFuncionarios = daoFuncionario.getByNome(nome);
-        
-        for(Funcionario f: listFuncionarios){
-            if(!f.getNome().equals("Administrador")){
+
+        for (Funcionario f : listFuncionarios) {
+            if (!f.getNome().equals("Administrador")) {
                 Map<String, String> specFuncionario = new HashMap();
 
                 specFuncionario.put("id", String.valueOf(f.getIdPessoa()));
@@ -129,54 +128,63 @@ public class ControllerFuncionario {
 
                 funcionarios.add(specFuncionario);
             }
-            
+
         }
-        
+
         return funcionarios;
-        
+
     }
 
     /*
      * @author dhiego and ...
      * This method creates and persists a Funcioanrio Object
      */
-    public static boolean cadastrar(String usuario, String senha, Acesso acesso,
-            String nome, String email, Calendar dataNascimento, Sexo sexo,
-            String[] numerosTel, String[] dddsTel, TipoTelefone[] tiposTel,
-            String estadoNome, String estadoUF, String cidadeNome,
-            String bairroNome, String logradouro, String numero,
-            String cep, String numeroDocumento, TipoDocumento tipoDocumento) {
+    public static boolean cadastrar(String nome, String email,
+            Calendar dataNascimento, Sexo sexo, String[] numerosTel,
+            String[] dddsTel, TipoTelefone[] tiposTel, String estadoUF,
+            String cidadeNome, String bairroNome, String logradouro, String numero,
+            String cep, String numeroDocumento, TipoDocumento tipoDocumento,
+            String usuario, String senha, Acesso acesso
+    ) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 
-        Estado estado = new Estado(estadoNome, estadoUF);
         Cidade cidade = new Cidade(cidadeNome);
-        cidade.setEstado(estado);
         Bairro bairro = new Bairro(bairroNome);
-        bairro.setCidade(cidade);
         Endereco endereco = new Endereco(logradouro, numero, cep);
         Documento documento = new Documento(numeroDocumento, TipoDocumento.CPF);
-        Funcionario funcionario = new Funcionario(usuario, senha, acesso, nome, email, dataNascimento, sexo);
-        funcionario.setDocumento(documento);
+
+        /*Hash Senha*/
+        HashSha hashSenha = new HashSha(senha);
+        senha = hashSenha.hashSenha();
+
+        Funcionario funcionario = new Funcionario(
+                usuario, senha, acesso, nome, email, dataNascimento, sexo);
+
+        ControllerEstado.getEstadoByUf(estadoUF).addCidade(cidade);
+        cidade.addBairro(bairro);
+        bairro.addEndereco(endereco);
         funcionario.setEndereco(endereco);
-        
-        for(int i = 0; i < numerosTel.length; i++){
+        funcionario.setDocumento(documento);
+
+        for (int i = 0; i < numerosTel.length; i++) {
             Telefone telefone = new Telefone(dddsTel[i], numerosTel[i], tiposTel[i]);
             funcionario.addTelefone(telefone);
         }
+
         return daoFuncionario.merge(funcionario);
     }
-    
-    public static boolean novaSenha(Long id, String senha) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+
+    public static boolean novaSenha(Long id, String senha) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         Funcionario funcionario = daoFuncionario.getById(id);
-        
+
         HashSha hashSenha = new HashSha(senha);
         senha = hashSenha.hashSenha();
-        
+
         funcionario.setSenha(senha);
-        
+
         return daoFuncionario.merge(funcionario);
     }
-    
-    public static boolean deleteFuncionario(Long id){
+
+    public static boolean deleteFuncionario(Long id) {
         return daoFuncionario.removeById(id);
     }
 
@@ -191,10 +199,48 @@ public class ControllerFuncionario {
         }
         return cal;
     }
-    
-    public static List<Funcionario> findByName(String nome){        
+
+    public static List<Funcionario> findByName(String nome) {
         FuncionarioDAO dao = new FuncionarioDAO();
         return null; //dao.getByName(nome);
+    }
+    
+    public static boolean editarFuncionario(Long id, String nome, String email,
+        Calendar dataNascimento, Sexo sexo, String[] numerosTel, 
+        String[] dddsTel, TipoTelefone[] tiposTel, String estadoUF,
+        String cidadeNome, String bairroNome, String logradouro, String numero,
+        String cep, String numeroDocumento,String usuario, Acesso acesso){
+        
+        Funcionario funcionario = daoFuncionario.getById(id);
+        
+        funcionario.setNome(nome);
+        funcionario.setEmail(email);
+        funcionario.setDataNascimento(dataNascimento);
+        funcionario.setSexo(sexo);
+        funcionario.getEndereco().getBairro().setNome(bairroNome);
+        funcionario.getEndereco().getBairro().getCidade().setEstado(ControllerEstado.getEstadoByUf(estadoUF));
+        funcionario.getEndereco().getBairro().getCidade().setNome(cidadeNome);
+        funcionario.getEndereco().getBairro().setNome(bairroNome);
+        funcionario.getEndereco().setLogradouro(logradouro);
+        funcionario.getEndereco().setNumero(numero);
+        funcionario.getEndereco().setCep(cep);
+        funcionario.getDocumento().setNumero(numeroDocumento);
+        funcionario.setUsuario(usuario);
+        funcionario.setAcesso(acesso);
+        
+        if(numerosTel.length == 1){
+            funcionario.getTelefones().get(0).setDdd(dddsTel[0]);
+            funcionario.getTelefones().get(0).setNumero(numerosTel[0]);
+        }
+        else{
+            funcionario.getTelefones().get(0).setDdd(dddsTel[0]);
+            funcionario.getTelefones().get(0).setNumero(numerosTel[0]);
+            
+            funcionario.getTelefones().get(1).setDdd(dddsTel[1]);
+            funcionario.getTelefones().get(1).setNumero(numerosTel[1]);
+        }
+        
+        return daoFuncionario.merge(funcionario);
     }
 
 }
